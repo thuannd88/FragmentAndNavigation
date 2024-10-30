7661ba0c9d5fffb126ec6ffdd3e183bdf4af13a8
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2024-10-30 14:07:51 +0700

    second commit ( Navigation with NavGraph) - We use NavGraph instead of declaring relation between menu and fragment directly in src code

---
+++ app/src/main/java/com/example/fragmentandnavigation/NavGraphActivity.kt
@@ -0,0 +1,25 @@
+package com.example.fragmentandnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
+
+class NavGraphActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_graph)
+
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+    }
+}
